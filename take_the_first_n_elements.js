a0ab56340596743c4ccc4cd5cb12be8cb824285c
Crew Spence <csspence123@gmail.com>
2020-01-19 06:19:56 -0600

    Wrote working solution for take_the_first_n_elements.

--- take_the_first_n_elements.js
+++ take_the_first_n_elements.js
@@ -7,6 +7,6 @@ If you need help, here's a reference:
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
 */
 
-function take(arr, n) {
-  // Your code here
+const take = (arr, n) => {
+  return arr.slice(0, n);
 }
